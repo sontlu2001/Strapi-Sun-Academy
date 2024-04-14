8ef9ce8d3f4ac898f552706e69628f29b79e272c
Do Son <sonnvc2001@gmail.com>
2024-04-14 22:34:00 +0700

    FEATE: Create API Learning Page

--- src/api/lesson/services/lesson.js
+++ src/api/lesson/services/lesson.js
@@ -8,6 +8,18 @@ const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::lesson.lesson", ({ strapi }) => ({
   async getLessonByCourse(courseId) {
-    return await strapi.db.query('api::lesson.lesson').findOne();
+    const lessons = await strapi.db.query('api::lesson.lesson').findMany({
+      select: ['name', 'resource', 'URL_video'],
+      where: {course_id: courseId},
+      orderBy: { id: 'ASC' },
+      populate: { chapter_id: true, course_id: true }
+    });
+    const chapters = await strapi.db.query('api::chapter.chapter').findMany({
+      where:  {course_id: courseId},
+    })
+    return {
+      chapters,
+      lessons
+    }
   },
 }));
